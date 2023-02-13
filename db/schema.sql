54af1e8ef4cd74f0d4541dd00c9c48d5e0bdd18e
Katherine Lantto <kathlantto@gmail.com>
2023-02-12 21:09:51 -0700

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,31 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE company_db;
+USE employee_db;
 
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  emp_name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    dept_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NULLIF("manager_id", "")
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
